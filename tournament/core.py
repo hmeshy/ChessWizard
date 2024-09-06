b6c32402a3478b620ca9939ebbcbe7bb999f81a7
MathIsFun0 <89877664+MathIsFun0@users.noreply.github.com>
2024-09-05 20:10:03 -0400

    basic elo calcs

--- tournament/core.py
+++ tournament/core.py
@@ -1,3 +1,6 @@
+import math
+from scipy.special import erfc, erfcinv
+
 # right now, only handles Base+Increment, probably fine simplification for most things
 class BasicTimeControl:
     base = 15 # in minutes
@@ -29,3 +32,40 @@ class Player:
             return self.rating_rapid
         else:
             return self.rating_blitz
+        
+# calc based on https://wismuth.com/elo/calculator.html
+        
+FIDE_STDDEV = 2000 / 7
+
+def eloNormal(eloDiff):
+    return erfc(-eloDiff / (FIDE_STDDEV * math.sqrt(2))) / 2
+
+def invEloNormal(p):
+    return -(FIDE_STDDEV * math.sqrt(2)) * erfcinv(p * 2)
+
+def addElo(diff, delta):
+    if diff * delta >= 0:
+        return diff + delta
+    else:
+        if diff > 0:
+            return -invEloNormal(2 * eloNormal(-diff) - eloNormal(-diff + delta))
+        else:
+            return invEloNormal(2 * eloNormal(diff) - eloNormal(diff - delta))
+
+def shiftedDiffs(white, black):
+    diff = white - black
+    ave = (white + black) / 2
+    eloPerPawn = math.exp(ave/1020)*26.58903951991242
+    c1 = eloPerPawn * 0.1 # win odds
+    c2 = eloPerPawn * 0.6 # win+draw odds
+    return (addElo(diff, c1-c2), addElo(diff, c1+c2))
+
+def eloDiff(white, black):
+    diff = white - black
+    diffs = shiftedDiffs(white, black)
+    if (diffs[0] > 0):
+        return -invEloNormal((eloNormal(-diffs[0]) + eloNormal(-diffs[1]))/2)
+    else:
+        return invEloNormal((eloNormal(diffs[0]) + eloNormal(diffs[1]))/2)
+
+def simGame(white, black, time):
